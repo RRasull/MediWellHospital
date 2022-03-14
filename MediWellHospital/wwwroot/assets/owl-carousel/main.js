a8dda27fd68cb84b0f72cde370558986af593db6
RRasull <92165344+RRasull@users.noreply.github.com>
2022-03-14 20:44:03 +0400

    'refactor changed assets from -wroot'

--- MediWellHospital/wwwroot/assets/owl-carousel/main.js
+++ MediWellHospital/wwwroot/assets/owl-carousel/main.js
@@ -15,3 +15,89 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+
+
+$(document).ready(function() {
+    $("#testimonial-slider").owlCarousel({
+      items: 2,
+      itemsDesktop: [1000, 2],
+      itemsDesktopSmall: [990, 2],
+      itemsTablet: [768, 1],
+      pagination: true,
+      navigation: false,
+      navigationText: ["", ""],
+      slideSpeed: 1000,
+      autoPlay: true
+    });
+  });
+
+
+  
+jQuery(function ($) {
+    "use strict";
+    var $window = $(window);
+    var windowsize = $(window).width();
+    var $root = $("html, body");
+    var $this = $(this);
+ 
+ 
+    /*Testimonials 3columns*/
+    $("#testimonial-slider").owlCarousel({
+       items: 3,
+       autoplay: 2500,
+       autoplayHoverPause: true,
+       loop: true,
+       margin: 30,
+       dots: true,
+       nav: false,
+       responsive: {
+          1280: {
+             items: 3,
+          },
+          600: {
+             items: 2,
+          },
+          320: {
+             items: 1,
+          },
+       }
+    });
+ 
+ 
+ });
+ 
+ 
+
+ jQuery(function ($) {
+    "use strict";
+    var $window = $(window);
+    var windowsize = $(window).width();
+    var $root = $("html, body");
+    var $this = $(this);
+  
+  
+    /*Testimonials 3columns*/
+    $("#testimonial-slider").owlCarousel({
+       items: 3,
+       autoplay: 2500,
+       autoplayHoverPause: true,
+       loop: true,
+       margin: 30,
+       dots: true,
+       nav: false,
+       responsive: {
+          1280: {
+             items: 3,
+          },
+          600: {
+             items: 2,
+          },
+          320: {
+             items: 1,
+          },
+       }
+    });
+  
+  
+ });
